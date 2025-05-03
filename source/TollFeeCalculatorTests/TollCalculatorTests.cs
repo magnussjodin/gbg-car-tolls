d5232d7bf1db01c3402fd85904cc9b05afc4bc5b
Magnus Sjödin <magnus.sjodin@gmail.com>
2025-05-03 16:48:14 +0200

    Add ShouldReturn0IfDateHasNoFee test

--- source/TollFeeCalculatorTests/TollCalculatorTests.cs
+++ source/TollFeeCalculatorTests/TollCalculatorTests.cs
@@ -19,6 +19,28 @@ public class TollCalculatorTests
         // Assert
         Assert.Equal(0, fee);
     }
+        
+    [Fact]
+    public void GetTimelyTollFee_ShouldReturn0IfDateHasNoFee()
+    {
+        var tollCalculator = new TollCalculator();
+        var vehicle = GetVehicleThatHasFee();
+
+        // Saturdays and Sundays should have no fee
+        var aSaturday = Enumerable.Range(1, 7)
+            .Select(day => new DateTime(2013, 2, 1).AddDays(day - 1))
+            .First(date => date.DayOfWeek == DayOfWeek.Saturday); // First Saturday of February 2013
+        Assert.Equal(0, tollCalculator.GetTimelyTollFee(aSaturday, vehicle));
+
+        var aSunday = aSaturday.AddDays(1); // First Sunday of February 2013
+        Assert.Equal(0, tollCalculator.GetTimelyTollFee(aSunday, vehicle));
+
+        // Some weekdays have no fee
+        foreach (var weekDay in GetWeekDaysThatHasNoFee())
+        {
+            Assert.Equal(0, tollCalculator.GetTimelyTollFee(weekDay, vehicle));
+        }
+    }
     
     [Theory]
     [MemberData(nameof(GetVehicleAndHasFeeTestData))]
@@ -89,5 +111,32 @@ public class TollCalculatorTests
         yield return new object[] { new DateTime(2013, 1, 2, 17, 0, 0), new DateTime(2013, 1, 2, 17, 59, 0), 13 };
         yield return new object[] { new DateTime(2013, 1, 2, 18, 0, 0), new DateTime(2013, 1, 2, 18, 29, 0), 8 };
         yield return new object[] { new DateTime(2013, 1, 2, 18, 30, 0), new DateTime(2013, 1, 2, 23, 59, 0), 0 };
+    }
+
+    public static IEnumerable<DateTime> GetWeekDaysThatHasNoFee()
+    {
+        yield return new DateTime(2013, 1, 1, 0, 0, 0);
+        yield return new DateTime(2013, 3, 28, 0, 0, 0);
+        yield return new DateTime(2013, 3, 29, 0, 0, 0);
+        yield return new DateTime(2013, 4, 1, 0, 0, 0);
+        yield return new DateTime(2013, 4, 30, 0, 0, 0);
+        yield return new DateTime(2013, 5, 1, 0, 0, 0);
+        yield return new DateTime(2013, 5, 8, 0, 0, 0);
+        yield return new DateTime(2013, 5, 9, 0, 0, 0);
+        yield return new DateTime(2013, 6, 5, 0, 0, 0);
+        yield return new DateTime(2013, 6, 6, 0, 0, 0);
+        yield return new DateTime(2013, 6, 21, 0, 0, 0);
+        yield return new DateTime(2013, 11, 1, 0, 0, 0);
+        yield return new DateTime(2013, 12, 24, 0, 0, 0);
+        yield return new DateTime(2013, 12, 25, 0, 0, 0);
+        yield return new DateTime(2013, 12, 26, 0, 0, 0);
+        yield return new DateTime(2013, 12, 31, 0, 0, 0);
+        // Every week day in July
+        foreach (var date in Enumerable.Range(1, 31)
+            .Select(day => new DateTime(2013, 7, 1).AddDays(day - 1))
+            .Where(date => date.DayOfWeek != DayOfWeek.Saturday && date.DayOfWeek != DayOfWeek.Sunday))
+        {
+            yield return date;
+        }
     }
 }
