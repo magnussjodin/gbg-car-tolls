e9f7a18650b321245cfaedf7b9a24cbc682b3d8a
Magnus Sjödin <magnus.sjodin@gmail.com>
2025-05-03 15:37:52 +0200

    Simplified GetTimelyTollFee logic for fee scheme

--- source/TollFeeCalculator/TollCalculator.cs
+++ source/TollFeeCalculator/TollCalculator.cs
@@ -59,15 +59,15 @@ public class TollCalculator
         int hour = date.Hour;
         int minute = date.Minute;
 
-        if (hour == 6 && minute >= 0 && minute <= 29) return 8;
-        else if (hour == 6 && minute >= 30 && minute <= 59) return 13;
-        else if (hour == 7 && minute >= 0 && minute <= 59) return 18;
-        else if (hour == 8 && minute >= 0 && minute <= 29) return 13;
-        else if (hour >= 8 && hour <= 14 && minute >= 0 && minute <= 59) return 8;
-        else if (hour == 15 && minute >= 0 && minute <= 29) return 13;
-        else if (hour == 15 && minute >= 0 || hour == 16 && minute <= 59) return 18;
-        else if (hour == 17 && minute >= 0 && minute <= 59) return 13;
-        else if (hour == 18 && minute >= 0 && minute <= 29) return 8;
+        if (hour == 6 && minute <= 29) return 8;
+        else if (hour == 6) return 13;
+        else if (hour == 7) return 18;
+        else if (hour == 8 && minute <= 29) return 13;
+        else if (hour >= 8 && hour <= 14) return 8;
+        else if (hour == 15 && minute <= 29) return 13;
+        else if (hour >= 15 && hour <= 16) return 18;
+        else if (hour == 17) return 13;
+        else if (hour == 18 && minute <= 29) return 8;
         else return 0;
     }
 
