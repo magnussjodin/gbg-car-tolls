e0fc57bcbe0a6639dd8cb917ebcba996a35bfd63
Magnus Sjödin <magnus.sjodin@gmail.com>
2025-05-04 08:28:57 +0200

    Add class for handling registering and getting out toll info about vehicle passages

---
+++ source/TollFeeCalculator/PassageTollHandler.cs
@@ -0,0 +1,19 @@
+namespace TollFeeCalculator
+{
+    /***
+     * Class to handle toll fee calculation for vehicles passing through a toll booth
+     * It is stateful, keeping track of every passage registered, something you can also get listed out.
+     * It can also give out lists of calculated toll fees for vehicles based on their type, date and time of passage.
+     */
+
+    public class PassageTollHandler
+    {
+
+        public PassageTollHandler()
+        {
+            // Constructor logic here
+        }
+
+        // Add methods and properties as needed
+    }
+}
---
+++ source/TollFeeCalculatorTests/PassageTollHandlerTests.cs
@@ -0,0 +1,10 @@
+using System;
+using Xunit;
+
+namespace TollFeeCalculatorTests
+{
+    public class PassageTollHandlerTests
+    {
+        // Add test methods here to validate the functionality of PassageTollHandler
+    }
+}
