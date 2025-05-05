aed588f6c6d446e57d70d080ee4fb20ad66fe63c
Magnus Sjödin <magnus.sjodin@gmail.com>
2025-05-05 11:38:58 +0200

    First app version, where a passage can be registered and passages/daily fees can be filtered out But there are some bugs to fix...

--- source/VehiclePassageTolls/MainWindow.xaml.cs
+++ source/VehiclePassageTolls/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,16 +11,68 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TollFeeCalculator;
 
-namespace VehiclePassageTolls;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace VehiclePassageTolls
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        private readonly PassageTollHandler _passageTollHandler;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _passageTollHandler = new PassageTollHandler();
+        }
+
+        private void RegisterPassage_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var licenseNumber = LicenseNumberInput.Text;
+                var vehicleType = (VehicleType)Enum.Parse(typeof(VehicleType), (VehicleTypeInput.SelectedItem as ComboBoxItem)?.Content.ToString() ?? "Car");
+                var date = TimestampInputDate.SelectedDate ?? DateTime.Now.Date;
+                var time = TimeSpan.Parse(TimestampInputTime.Text);
+
+                var timestamp = date.Add(time);
+
+                _passageTollHandler.RegisterPassage(licenseNumber, vehicleType, timestamp);
+                Filter_Click(sender, e);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void Filter_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var licenseNumber = FilterLicenseNumberInput.Text;
+                var fromDate = FilterFromDate.SelectedDate ?? DateTime.MinValue;
+                var toDate = FilterToDate.SelectedDate ?? DateTime.MaxValue;
+
+                // Filter passages
+                IOrderedEnumerable<VehiclePassage> passages;
+                if (string.IsNullOrEmpty(licenseNumber))
+                    passages = _passageTollHandler.GetListOfPassages(fromDate, toDate);
+                else
+                    passages = _passageTollHandler.GetListOfPassages(licenseNumber, fromDate, toDate);
+                PassagesDataGrid.ItemsSource = passages.ToList();
+
+                // Filter daily fees
+                IOrderedEnumerable<DailyFee> dailyFees;
+                if (string.IsNullOrEmpty(licenseNumber))
+                    dailyFees = _passageTollHandler.GetListOfDailyFees(fromDate, toDate);
+                else
+                    dailyFees = _passageTollHandler.GetListOfDailyFees(licenseNumber, fromDate, toDate);
+                DailyFeesDataGrid.ItemsSource = dailyFees.ToList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
     }
 }
