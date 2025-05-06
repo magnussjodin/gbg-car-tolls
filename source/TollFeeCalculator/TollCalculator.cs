d52e98da50bd2c4767b63c0fc932eb2484642ce9
Magnus Sjödin <magnus.sjodin@gmail.com>
2025-05-06 09:53:15 +0200

    Lifted out from 2013 part, the toll fee date logic that is common for any year

--- source/TollFeeCalculator/TollCalculator.cs
+++ source/TollFeeCalculator/TollCalculator.cs
@@ -150,16 +150,21 @@ public class TollCalculator
 
         if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday) return true;
 
+        if (month == 1 && day == 1 || // New Year's Day
+            month == 6 && day == 6 || // National Day
+            month == 7 || // July
+            month == 12 && (day == 24 || day == 25 || day == 26 || day == 31)) // Christmas or New Year's Eve
+        {
+            return true;
+        }
+
         if (year == 2013)
         {
-            if (month == 1 && day == 1 ||
-                month == 3 && (day == 28 || day == 29) ||
+            if (month == 3 && (day == 28 || day == 29) ||
                 month == 4 && (day == 1 || day == 30) ||
                 month == 5 && (day == 1 || day == 8 || day == 9) ||
-                month == 6 && (day == 5 || day == 6 || day == 21) ||
-                month == 7 ||
-                month == 11 && day == 1 ||
-                month == 12 && (day == 24 || day == 25 || day == 26 || day == 31))
+                month == 6 && (day == 5 || day == 21) ||
+                month == 11 && day == 1)
             {
                 return true;
             }
